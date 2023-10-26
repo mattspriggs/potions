82d66a6d15d48b621349a9826c2c9112238c902c
Matt Spriggs <83927528+mattspriggs@users.noreply.github.com>
2023-10-26 17:34:41 +1300

    Blender migration done

--- server/db/migrations/20231026041234_blender.js
+++ server/db/migrations/20231026041234_blender.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
-
+export async function up(knex) {
+  await knex.schema.createTable('blender', (table) => {
+    table.increments('blender_id').primary()
+    table.string('blender_name')
+    table.string('blender_image_url')
+    table.string('blender_description')
+  })
+}
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+export async function down(knex) {
+  await knex.schema.dropTable('blender')
+}
