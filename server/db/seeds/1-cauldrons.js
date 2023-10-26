6bb775768701838ab10a7a9e94424b1fd8def4e8
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-10-27 10:57:20 +1300

    Data added to cauldrons seed file

--- server/db/seeds/1-cauldrons.js
+++ server/db/seeds/1-cauldrons.js
@@ -8,27 +8,27 @@ export async function seed(knex) {
   await knex('cauldrons').insert([
     {
       cauldrons_id: 1,
-      cauldrons_name: 'Jatin',
-      cauldrons_image_url: 'rowValue2',
-      cauldrons_description: 'rowValue3',
+      cauldrons_name: 'Potion Brewer Jatin',
+      cauldrons_image_url: '../images/jatincauldron.png',
+      cauldrons_description: 'Brewing the only Force potions available in this interstellar quadrant!  Get your Midichlorian booster now from Jedi Jatin!',
     },
     {
       cauldrons_id: 2,
-      cauldrons_name: 'David',
-      cauldrons_image_url: 'rowValue2',
-      cauldrons_description: 'rowValue3',
+      cauldrons_name: 'Potion Brewer David',
+      cauldrons_image_url: '../images/Davidcauldron.png',
+      cauldrons_description: 'Brewing the best enhancement potions available!  Get yours from the Board Game Guru David today!',
     },
     {
       cauldrons_id: 3,
-      cauldrons_name: 'Jen',
-      cauldrons_image_url: 'rowValue2',
-      cauldrons_description: 'rowValue3',
+      cauldrons_name: 'Potion Brewer Jen',
+      cauldrons_image_url: '../images/jencauldron.png',
+      cauldrons_description: 'Brewing the slinkiest of stealth and pur postions available!  Visit Cat Whisperer Jen for all your Feline potions!',
     },
     {
       cauldrons_id: 4,
-      cauldrons_name: 'DaviD',
-      cauldrons_image_url: 'rowValue2',
-      cauldrons_description: 'rowValue3',
+      cauldrons_name: 'Potion Brewer DaviD',
+      cauldrons_image_url: '../images/Daveedcauldron.png',
+      cauldrons_description: 'Brewing the finest elemental potions!  Get yours now from the Last Airbender!',
     },
   ])
 }
