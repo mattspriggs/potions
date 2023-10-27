947c2e4fe66a68318d218c7806106022d0646697
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-10-27 13:33:07 +1300

    Added random adjectives name array, working on images array

--- server/db/seeds/4-random_adjectives.js
+++ server/db/seeds/4-random_adjectives.js
@@ -6,8 +6,9 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('random_adjectives').del()
   await knex('random_adjectives').insert([
-    { id: 1, randoms_name: 'rowValue1', randoms_cauldron_id: 'rowValue2', randoms_image_url: 'rowValue3' },
-    { id: 2, randoms_name: 'rowValue1', randoms_cauldron_id: 'rowValue2', randoms_image_url: 'rowValue3'},
-    { id: 3, randoms_name: 'rowValue1', randoms_cauldron_id: 'rowValue2', randoms_image_url: 'rowValue3'},
+    { randoms_name: ['Forceful','Interstellar', 'Galactic', 'Rebel'], randoms_cauldron_id: 1, randoms_image_url: 'rowValue3' },
+    { randoms_name: ['Tactical', 'Strategic', 'Calculating', 'Grandmaster'], randoms_cauldron_id: 2, randoms_image_url: 'rowValue3'},
+    { randoms_name: ['Pouncing', 'Playful', 'Stretching', 'Sneaky'], randoms_cauldron_id: 3, randoms_image_url: 'rowValue3'},
+    { randoms_name: ['Avartic', 'Harmonizing', 'Masterful', 'Balancing'], randoms_cauldron_id: 4, randoms_image_url: 'rowValue3'},
   ])
 }
